a7cb129b3f5a5398255a509d9781a8189b6b4f2f
Jared Jolton <jared.jolton@gmail.com>
2017-09-14 23:33:07 -0600

    working ondropzone

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request
 import inspect
 from collections import defaultdict
 from flask_dropzone import Dropzone
-
+import os
 
 app = application = Flask("motionandvibration", static_url_path='')
 dropzone = Dropzone(app)
@@ -12,13 +12,13 @@ calls = defaultdict(lambda: 0)
 @app.route("/", methods=["GET"])
 def index():
     calls[inspect.stack()[0][3]] += 1
-    return render_template('index.html', dropzone = dropzone), 200
+    return render_template('index.html'), 200
 
 @app.route("/uploadz", methods=["POST"])
 def uploadz():
     calls[inspect.stack()[0][3]] += 1
-    files = request.files.getlist('file')
-    print (files)
+    for f in request.files.getlist('input_name'):
+        f.save(os.path.join("~/Desktop", f.filename))
     return "", 200
 
 @app.route("/healthz", methods=["GET"])
